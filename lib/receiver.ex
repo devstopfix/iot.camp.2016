281175277491de5204eddf910bd7e287b6dfc06a
James Every <james.every@zuhlke.com>
2016-09-19 17:52:52 +0200

    Receiver

---
+++ lib/receiver.ex
@@ -0,0 +1,12 @@
+defmodule Receiver do
+  use Hulaaki.Client
+
+  @qos   1
+  @topic "IOT"
+  @options [client_id: "iot-test", host: "localhost", port: 1883]
+
+  def on_subscribed_publish(options) do
+    IO.inspect options
+  end
+
+end
--- test/publish_client_test.exs
+++ test/publish_client_test.exs
@@ -8,6 +8,8 @@ defmodule PublishClientTest do
       {:ok, mqtt: pid}
   end
 
+  #on_exit PublishClient.disconnect
+
   test "We can publish JSON to a topic", state do
   	pid = state[:mqtt]
     PublishClient.publish_json(pid, %{:hello=>"World"})
