0ded42af3fb8a94e7b590b1c35f8d3fbea696304
Raiyan Kabir <raiyan.kabir@zuhlke.com>
2016-09-22 12:01:12 +0200

    Fix for the receiver test setup

--- test/receiver_test.exs
+++ test/receiver_test.exs
@@ -2,26 +2,26 @@ defmodule ReceiverTest do
   use ExUnit.Case
   doctest Receiver
 
-  # setup_all do
-  # 	  {:ok, r} = Receiver.start_link(%{})
-  #     Receiver.connect(r, [client_id: "iot-test", host: "localhost", port: 1883])
+  setup_all do
+  	  {:ok, r} = Receiver.start_link(%{})
+      Receiver.connect(r, [client_id: "iot-receiver", host: "localhost", port: 1883])
 
-  # 	  {:ok, p} = PublishClient.start_link(%{})
-  #     PublishClient.connect(p, [client_id: "iot-test", host: "localhost", port: 1883])
+  	  {:ok, p} = PublishClient.start_link(%{})
+      PublishClient.connect(p, [client_id: "iot-publisher", host: "localhost", port: 1883])
 
-  #     {:ok, %{pub: p, rec: r}}
-  # end
+      {:ok, %{pub: p, rec: r}}
+  end
 
-  # test "We can subscribe to a topic", state do
-  # 	pid = state[:pub]
-  #   Receiver.subscribe(pid, [id: 24_756, topics: ["IOT"], qoses: [1]])
-  # end
+  test "We can subscribe to a topic", state do
+  	pid = state[:pub]
+    Receiver.subscribe(pid, [id: 24_756, topics: ["IOT"], qoses: [1]])
+  end
 
-  # test "We can retrieve a JSON message from a topic", state do
-  #   PublishClient.publish_json(state[:pub], %{:hello=>"World"})
-  #   PublishClient.disconnect(state[:pub])
+  test "We can retrieve a JSON message from a topic", state do
+    PublishClient.publish_json(state[:pub], %{:hello=>"World"})
+    PublishClient.disconnect(state[:pub])
 
-  #   Receiver.subscribe(state[:rec], [id: 24_756, topics: ["IOT"], qoses: [1]])
-  # end
+    Receiver.subscribe(state[:rec], [id: 24_756, topics: ["IOT"], qoses: [1]])
+  end
 
 end
